794f420c15e3ade7b1b7d9d70716db01b1de5ab6
David Pérez-Suárez <d.perez-suarez@ucl.ac.uk>
2025-01-23 10:31:45 +0000

    Pits are dangerous

--- guacamole.py
+++ guacamole.py
@@ -2,6 +2,6 @@ from fruits import avocado, lime
 from condiment import salt, olive_oil
 
 def guacamole(people=1):
-    bowl = avocado.peel() + lime.drop(2) + salt.pinch() + olive_oil.ts()
+    bowl = avocado.peel().unseed() + lime.drop(2) + salt.pinch() + olive_oil.ts()
     return bowl.blend() * people
 
