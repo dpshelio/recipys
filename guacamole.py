3ea6cc1a97467ca20fd53cb33af54483aea74c85
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2025-01-23 10:27:22 +0000

    Guacamole converted to its traditional recipe

--- guacamole.py
+++ guacamole.py
@@ -1,7 +1,7 @@
-from fruits import avocado, lemon
+from fruits import avocado, lime
 from condiment import salt, olive_oil
 
 def guacamole(people=1):
-    bowl = avocado.peel() + lemon.drop(2) + salt.pinch() + olive_oil.ts()
+    bowl = avocado.peel() + lime.drop(2) + salt.pinch() + olive_oil.ts()
     return bowl.blend() * people
 
